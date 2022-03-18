d40d8bf3974194939aebd5750dc70e68e9fe9c89
Jacob Wærum Lauritzen <jwla42951@edu.ucl.dk>
2022-03-18 15:30:24 +0100

    Opdatering

--- Team 3 - Mini Projekt/Team 3 - Mini Projekt/LoadFile.cs
+++ Team 3 - Mini Projekt/Team 3 - Mini Projekt/LoadFile.cs
@@ -40,20 +40,32 @@ namespace Team_3___Mini_Projekt
 
             for (int i = 0; i < listNumber; i++)
             {
+                int stringLength = list[i].Length;
                 int zeroComma = 0;
                 int firstComma = list[i].IndexOf(",");
-                int secoundComma = list[i].IndexOf(",", list[i].IndexOf(",") + 1);
+                int secoundComma = list[i].IndexOf(",", firstComma+1);
+                int thirdComma = list[i].IndexOf(",", secoundComma+1);
+                int forthComma = list[i].IndexOf(",", thirdComma + 1);
+                //Til at teste hvad der er i variablerne
+                Console.WriteLine("hvad står der i list nummer [{1}] : [{0}]",list[i],i);
+                Console.WriteLine("Hvar er der i variablerne: \n stringLength={5} og zeroComma={0} og firstComma={1} og secoundComma={2} og thirdComma={3} og forthComma={4}", zeroComma,firstComma,secoundComma,thirdComma,forthComma,stringLength);
+                int calc = stringLength - thirdComma;
+                Console.WriteLine("Hvad er stringlængden{0} og hvad er stringlængden minus third{1}",stringLength,(stringLength - thirdComma));
+                Console.WriteLine("Hvad er stringlængden{0} og hvad er stringlængden minus forth{1}", stringLength, (stringLength - forthComma));
 
                 int gotID = int.Parse(list[i].Substring(zeroComma, firstComma));
                 string gotMODEL = list[i].Substring(firstComma+1, secoundComma-2);
+                string gotNUMBERPLATE = list[i].Substring(secoundComma+1, stringLength - forthComma);
+                string gotYEAR = list[i].Substring(thirdComma + 1, 4);
+
 
                 Cars.carStruct carobject = new Cars.carStruct();
 
                 carobject.ID = gotID;
                 carobject.Model = gotMODEL;
-                carobject.NumberPlate = "DG34288";
+                carobject.NumberPlate = gotNUMBERPLATE;
+                carobject.Year = int.Parse(gotYEAR);
                 carobject.Price = 344222.22;
-                carobject.Year = 1993;
 
                 Console.WriteLine("Hvad er ID?");
                 Console.WriteLine(gotID);
@@ -65,7 +77,7 @@ namespace Team_3___Mini_Projekt
             Console.WriteLine(carobjectList.Count);
             for (int i = 0;i < carobjectList.Count; i++)
             {
-                Console.WriteLine("ID: "+carobjectList[i].ID+" Model: "+carobjectList[i].Model + " Nummerplade: " + carobjectList[i].NumberPlate);
+                Console.WriteLine("ID: [{0}] , ",carobjectList[i].ID,carobjectList[i].Model,carobjectList[i].NumberPlate,carobjectList[i].Year);
             }
 
             // Console.WriteLine(car1.Model);
