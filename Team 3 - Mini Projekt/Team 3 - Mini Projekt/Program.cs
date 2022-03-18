e82823535c58083e33697f37faedaf30f585fdd9
Jens Bach <36878482+leaskeg@users.noreply.github.com>
2022-03-18 10:10:01 +0100

    Add files via upload

---
+++ Team 3 - Mini Projekt/Team 3 - Mini Projekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Team_3___Mini_Projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
