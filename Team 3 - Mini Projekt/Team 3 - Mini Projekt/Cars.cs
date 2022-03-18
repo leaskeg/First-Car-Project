ffc83c8719dd633e5b2e6828cab015e4948d3cd9
Jens Bach <leaskegaming@gmail.com>
2022-03-18 10:49:29 +0100

    HealeajrRevert "Merge branch 'main' of https://github.com/leaskeg/Team-3"
    This reverts commit bb3ac9de26def0333ed275335d3a721591850da0, reversing
    changes made to b7d71bbe1ec0d951b26f6a2a033bd7afd1e7f559.

--- Team 3 - Mini Projekt/Team 3 - Mini Projekt/Cars.cs
+++ Team 3 - Mini Projekt/Team 3 - Mini Projekt/Cars.cs
@@ -9,6 +9,15 @@ namespace Team_3___Mini_Projekt
     abstract class Cars
     {
         //kommentar
+        //<<<<<<< HEAD
+
+        //Et eller andet
+        public abstract string Name { get; }
+            public abstract string Description { get; } 
+                    
+        //=======
         private int id = 0;
+        //>>>>>>> 186d214990c6835a10862088879dcde5dc614863
+        //>>>>>>> 186d214990c6835a10862088879dcde5dc614863
     }
 }
